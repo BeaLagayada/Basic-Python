252eb4cd83fd9cc6ff905107b77b97361fa95024
BeaLagayada <144906586+BeaLagayada@users.noreply.github.com>
2023-09-13 04:44:25 -0700

    Add files via upload

---
+++ Act1_Lagayada.py
@@ -0,0 +1,22 @@
+#Creating My Complete Information
+
+FullName = "Bea Lagayada"
+Course = "Computer Science"
+Section = "CS 1E"
+Age = 20
+Birthday = "May 20, 2003"
+CompleteAddress = "Purok 1-B, Del Carmen, Iligan City"
+
+print("Complete Name: " + FullName)
+
+print("Course: " + Course)
+
+print("Section: " + Section)
+
+print("Age: ", Age)
+
+print("Birthday:" + Birthday)
+
+print("Complete Home Address: " + CompleteAddress)
+
+
