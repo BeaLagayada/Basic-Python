e33bbd1b13374c091c2e3eebe3f3a0af427f424d
BEA LAGAYADA <144906586+BeaLagayada@users.noreply.github.com>
2025-01-27 00:23:53 +0800

    Create Tutorial.py

---
+++ Tutorial.py
@@ -0,0 +1,87 @@
+first_name, age, pretty = "beaaaa", 20, True
+#print(first_name.find("e"))
+#print(first_name.capitalize())
+#print(first_name.upper())
+#print(first_name.lower())
+#print(first_name.isdigit())
+#print(first_name.isalpha())
+#print(first_name.count('a'))
+#print(first_name.replace("a",'l'))
+#print(first_name*4)
+
+#x = 1
+#y = 2.0
+#z = '3'
+
+#y = float(y)
+#z = float(z)
+
+#print('x is: '+str(x))
+#print(y)
+#print(z+10)
+
+#name = input("What is your name?: ")
+#age = int(input('How old are you?: '))
+#height = float(input('How tall are you?: '))
+
+#print("Hello "+name)
+#print('You are '+str(age)+' years old')
+#print("You are "+str(height)+ "cm tall")
+
+#import math
+#pi = 3.14
+#a = 90
+#y = 2
+#z = 3
+
+#print(round(pi))
+#print(math.ceil(pi))
+#print(math.floor(pi))
+#print(abs(pi))
+#print(pow(pi, 2))
+#print(math.sqrt(a))
+#print(max(a,y,z))
+#print(min(a,y,z,pi))
+
+#name ="Bea Lagayada"
+
+#fname = name[0:3]
+#lname = name[4:12]
+#sname = name[0:12:5]
+#reversed_name = name[ : : -7]
+
+#print(fname)
+#print(lname)
+#print(sname)
+#print(reversed_name)
+
+#slice function is use to slice object
+
+#website1 = "http://google.com"
+#website2 = "http://youtube.com"
+
+#slice = slice(7,-4,)
+
+#print(website1[slice])
+#print(website2[slice])
+
+
+#age = int(input("how old are you?: "))
+
+#if age == 100:
+    #print("You are a century old!")
+#elif age >= 18:
+    #print("You are an adult!")
+#elif age<0:
+    #print("You haven't been born yet!")
+#else:
+    #print("You are a child!")
+
+#temp = int(input("What is the temperature outside?: "))
+
+#if not(temp>=0 and temp <=30):
+    #print("the temperature is bad today!")
+    #print("stay inside!")
+#elif not (temp <0 or temp >30):
+    #print("the temperature is good today!")
+    #print("go outside!")
